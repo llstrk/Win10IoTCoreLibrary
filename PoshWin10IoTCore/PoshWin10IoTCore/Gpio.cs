037c33f66c3d2f1c34ba6b1a42b7699445612a66
llstrk <dryerr@gmail.com>
2016-02-23 23:45:45 +0100

    Basic functionality

---
+++ PoshWin10IoTCore/PoshWin10IoTCore/Gpio.cs
@@ -0,0 +1,12 @@
+﻿using Windows.Devices.Gpio;
+
+namespace PoshWin10IoTCore
+{
+    public class Gpio
+    {
+        public static GpioController GetGpioController()
+        {
+            return GpioController.GetDefault();
+        }
+    }
+}
