8765fb467375c60c7612fbb4c6c1459a39f27de3
llstrk <dryerr@gmail.com>
2016-02-24 00:17:51 +0100

    Renamed solution.

--- Win10IoTCoreLibrary/Win10IoTCoreLibrary/Properties/AssemblyInfo.cs
+++ Win10IoTCoreLibrary/Win10IoTCoreLibrary/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PoshWin10IoTCore")]
+[assembly: AssemblyTitle("Win10IoTCoreLibrary")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PoshWin10IoTCore")]
+[assembly: AssemblyProduct("Win10IoTCoreLibrary")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
